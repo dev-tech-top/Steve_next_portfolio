cdc0b225ae3dd346b052951a6b5b6688594e75b6
dev-tech-top <stevebayard226@gmail.com>
2025-06-18 03:22:50 -0400

    insert social icons

--- constants/social_media.js
+++ constants/social_media.js
@@ -1,4 +1,4 @@
-import { BsFillEnvelopeOpenFill, BsGithub, BsTwitter } from "react-icons/bs";
+import { BsTelegram, BsGithub, BsTwitterX, BsStackOverflow } from "react-icons/bs";
 
 export const SOCIAL_MEDIA = [
 	{
@@ -8,15 +8,22 @@ export const SOCIAL_MEDIA = [
 		url: "https://github.com/dev-tech-top"
 	},
 	{
+		id: "BsStackOverflow",
+		icon: <BsStackOverflow />,
+		title: "Visit Stack Overflow profile",
+		url: "https://stackoverflow.com/users/30721005/steve-bayard"
+	},
+	{
 		id: "BsTwitter",
-		icon: <BsTwitter />,
+		icon: <BsTwitterX />,
 		title: "Visit Github profile",
 		url: "https://x.com/steve_bayard226"
 	},
-	// {
-	// 	id: "BsTelegram",
-	// 	icon: <BsTelegram />,
-	// 	title: "Contact me on Telegram",
-	// 	url: "https://web.telegram.org/k/#@superDev226"
-	// },
+	{
+		id: "BsTelegram",
+		icon: <BsTelegram />,
+		title: "Contact me on Telegram",
+		url: "https://web.telegram.org/k/#@superDev226"
+	},
+
 ];
