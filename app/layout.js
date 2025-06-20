11bbb3cc2c32bc4f606224c05be88e2d0bc586ed
dev-tech-top <stevebayard226@gmail.com>
2025-06-20 16:17:33 -0400

    insert analytics

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,17 @@ export const metadata = { ...AppMetadata };
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-Q7DN85R4ZR"></script>
+				<script dangerouslySetInnerHTML={{
+					__html: `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', 'G-Q7DN85R4ZR');
+				`
+				}} />
+			</head>
 			<body>
 				<ThemeContext>
 					<AppHeader />
